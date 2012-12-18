bb6ea7d4727089bf3fcfb4bc7eb518ef2bd763da
amev7fam <ae_7fam@yahoo.com>
2012-12-18 14:02:19 -0700

    May as well get those defs fixed while we're at it.

--- MTGUI/MTGUI.cpp
+++ MTGUI/MTGUI.cpp
@@ -5,13 +5,22 @@
 //		Platforms:	Win32
 //		Processors: All
 //
-//	Copyright © 1999-2003 Yannick Delwiche. All rights reserved.
+//	Copyright ï¿½ 1999-2003 Yannick Delwiche. All rights reserved.
 //
 //---------------------------------------------------------------------------
-#include <windows.h>
+#ifdef _WIN32
+#	include <windows.h>
+#endif
 //---------------------------------------------------------------------------
+#ifdef _WIN32
 BOOL APIENTRY DllMain(HANDLE hModule,DWORD ul_reason_for_call,LPVOID lpReserved)
 {
 	return TRUE;
 }
+#else
+int main(int argc,const char* argv[])
+{
+	return 0;
+}
+#endif
 //---------------------------------------------------------------------------
