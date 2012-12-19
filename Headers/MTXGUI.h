97b207c4c8c52b73249ccf609e49e54693a4540d
amev7fam <ae_7fam@yahoo.com>
2012-12-18 20:29:38 -0700

    Start of GUI merge.

--- Headers/MTXGUI.h
+++ Headers/MTXGUI.h
@@ -2,7 +2,7 @@
 //
 //	MadTracker 3 Public Extension Header
 //
-//	Copyright © 1999-2006 Yannick Delwiche. All rights reserved.
+//	Copyright ï¿½ 1999-2006 Yannick Delwiche. All rights reserved.
 //
 //	http://www.madtracker.org/
 //	info@madtracker.org
@@ -161,6 +161,88 @@ public:
 	virtual bool MTCT isdragged(MTPoint &p1,MTPoint &p2) = 0;
 	virtual int MTCT getmetric(int metric) = 0;
 	virtual void MTCT setcontrolname(MTControl *ctrl,const char *prefix) = 0;
+        
+	bool active;
+	bool visible;
+
+/*	MTGUIInterface();
+	bool MTCT init();
+	void MTCT uninit();
+	void MTCT start();
+	void MTCT stop();
+	void MTCT processcmdline(void *params);
+	void MTCT showusage(void *out);
+	int MTCT config(int command,int param);
+	virtual int MTCT processmessages(bool userinput);
+	virtual int MTCT getnumcontrols();
+	virtual bool MTCT getcontroltype(int id,char *name,int &type);
+	virtual MTControl* MTCT newcontrol(int type,int tag,MTWinControl *parent,int l,int t,int w,int h,void *param);
+	virtual void MTCT delcontrol(MTControl *control);
+	virtual MTWindow* MTCT loadwindow(MTResources *res,int id,MTDesktop *desktop,bool autosave);
+	virtual bool MTCT savewindow(MTWindow *window);
+	virtual MTWindow* MTCT loadwindowfromfile(MTFile *f,int size,MTDesktop *desktop);
+	virtual int MTCT savewindowtofile(MTFile *f,MTWindow *window);
+	virtual void MTCT loadskin(MTResources *res);
+	virtual void* MTCT saveskin(int &size);
+	virtual void MTCT setdisplay(MTBitmap *s);
+	virtual void MTCT freedisplay();
+	virtual bool MTCT getdesign();
+	virtual void MTCT setdesign(bool d);
+	virtual MTDesktop* MTCT getdesktop(int id);
+	virtual int MTCT ctrltimer(MTControl *ctrl,int flags,int interval,bool immediate,bool accurate = false);
+	virtual void MTCT deltimer(MTControl *ctrl,int timerid);
+	virtual int MTCT synchronize(MTSync*);
+	virtual void* MTCT getimagelist(int id);
+	virtual Skin* MTCT getskin();
+	virtual void* MTCT createwindow(int l,int t,int w,int h,char *caption,int flags,void *parent);
+	virtual void MTCT deletewindow(void *wnd);
+	virtual void* MTCT getwindowproc();
+	virtual void* MTCT getappwindow();
+	virtual void MTCT monitordesktop(MTDesktop *dsk);
+	virtual void MTCT invalidaterect(void *wnd,MTRect &r);
+	virtual void MTCT invalidatergn(void *wnd,void *rgn);
+	virtual void MTCT windowaction(void *wnd,int action);
+	virtual void MTCT windowmove(void *wnd,int x,int y,bool relative);
+	virtual void MTCT resetcursor();
+	virtual float MTCT getcursorphase();
+	virtual MTControl* MTCT getfocusedcontrol();
+	virtual void MTCT setmouseshape(int cursor);
+	virtual void MTCT restoremouseshape();
+	virtual void MTCT getmousevector(float &x,float &y);
+	virtual void MTCT getmousepos(MTPoint &mp);
+	virtual void MTCT registershortcut(MTShortcut *s);
+	virtual void MTCT unregistershortcut(MTShortcut *s);
+	virtual int MTCT registershortcutgroup();
+	virtual void MTCT setgrid(int gx,int gy);
+	virtual void MTCT needbitmapcheck();
+	virtual void MTCT savewindowstate(void *wnd);
+	virtual void MTCT restorewindowstate(void *wnd);
+	virtual void MTCT getwindowrect(void *wnd,MTRect &rect,bool client);
+	virtual bool MTCT isdragged(MTPoint &p1,MTPoint &p2);
+	virtual int MTCT getmetric(int metric);
+	virtual void MTCT setcontrolname(MTControl *ctrl,const char *prefix);
+ * 
+ * Need to merge. List in comments above.
+ */
+private:
+	struct _MTTimer{
+		int flags;
+		MTControl *ctrl;
+		int id;
+		bool accurate;
+	};
+	bool isinit;
+	bool running;
+	int nsync;
+	int timercount;
+	MTEvent *runningevent;
+	MTHash *timers;
+	void sync(MTSync*);
+	static VOID CALLBACK TimerProc(HWND wnd,UINT uMsg,UINT idEvent,DWORD dwTime);
+	static LRESULT CALLBACK WindowProc(HWND wnd,unsigned int msg,unsigned int wparam,long lparam);
+	static void MTCT TimerProc2(MTTimer *timer,int param);
+	static int MTCT TimerSync(MTSync *s);
+	static void MTCT TimerDelete(void *item,void *param);
 };
 //---------------------------------------------------------------------------
 #endif
